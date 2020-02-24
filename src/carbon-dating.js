5411fa3e7563bef831251c0689c4aee3d68e66dc
abukato <artur.bukato@gmail.com>
2020-02-24 17:41:38 +0300

    feat: task carbon-dating done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,15 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+
+module.exports = function dateSample(sampleActivity) {
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  if (typeof sampleActivity === 'string') {
+    let sampleActivityNumber = parseFloat(sampleActivity);
+    if (isNaN(sampleActivityNumber)) return false;
+    if (sampleActivityNumber > 15 || sampleActivityNumber <= 0) return false;
+    return Math.ceil((Math.log(MODERN_ACTIVITY/sampleActivityNumber))/k);
+  } else {
+    return false;
+  }
 };
