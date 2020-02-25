32c4f47056fd01b32e6511e64b13f32d0644ff89
abukato <artur.bukato@gmail.com>
2020-02-25 17:59:52 +0300

    feat: task extended-repeater done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,31 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+    const main = String(str);
+    const addition =
+      typeof options.addition === "boolean" ||
+      options.addition === null ||
+      options.addition
+        ? String(options.addition)
+        : "";
+    const repeatTimes = options.repeatTimes || 1;
+    const additionRepeatTimes = options.additionRepeatTimes || 1;
+    const separator = options.separator ? String(options.separator) : "+";
+    const additionSeparator = options.additionSeparator
+      ? String(options.additionSeparator)
+      : "|";
   
+    let result = "";
+  
+    for (let i = 0; i < repeatTimes; i++) {
+      result += main;
+      for (let j = 0; j < additionRepeatTimes; j++) {
+        result += addition;
+        if (j + 1 < additionRepeatTimes) {
+          result += additionSeparator;
+        }
+      }
+      if (i + 1 < repeatTimes) {
+        result += separator;
+      }
+    }
+    return result;
+};
